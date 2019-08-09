006d69efbaa448464eec2c3b804b1edca90a6bf8
dotkalm <joelmholmberg@gmail.com>
2019-08-09 16:46:02 -0700

    logs form

--- src/Employees/index.js
+++ src/Employees/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-
+import CreateEmployee from './CreateEmployee';
 class Employee extends Component {
    state = {
       employees: []
    }
+   addEmployee = async (employee, e) => {
+      e.preventDefault();
+      console.log(employee, e, '<-- inside of add employee')
+   }
    render(){
       return(
          <div>
+            <CreateEmployee addEmployee={this.addEmployee}/>
             Sesame Street EMPLOYEES
          </div>
       )
