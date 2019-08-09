6e6f47a6584eb96ad10711da00365cc3fe453127
dotkalm <joelmholmberg@gmail.com>
2019-08-09 15:31:22 -0700

    add router and employee route

---
+++ src/Employees/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Employee extends Component {
+   state = {
+      employees: []
+   }
+   render(){
+      return(
+         <div>
+            Sesame Street EMPLOYEES
+         </div>
+      )
+   }
+}
+export default Employee
