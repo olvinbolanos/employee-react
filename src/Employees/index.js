d01d50fdeaa142daac588e08693512b121b8faa9
dotkalm <joelmholmberg@gmail.com>
2019-08-09 19:52:45 -0700

    added employee

--- src/Employees/index.js
+++ src/Employees/index.js
@@ -7,6 +7,28 @@ class Employee extends Component {
    addEmployee = async (employee, e) => {
       e.preventDefault();
       console.log(employee, e, '<-- inside of add employee')
+      try {
+         const createEmployee = await fetch('http://localhost:9000/api/v1/employee',{
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify(employee),
+            headers: {
+               'Content-Type': 'application/json'
+            }
+         })
+         console.log(createEmployee, '<-- create Employee fetch')
+         if(createEmployee.status !== 200){
+            throw Error('Resource no found')
+         }
+         const createEmployeeResponse = await createEmployee.json();
+         console.log(createEmployeeResponse.data, ' <-- createEmployeeResponse');
+         this.setState({
+            employees: [...this.state.employees, createEmployeeResponse.data]
+         })
+      } catch(err){
+         console.log(err, ' add employee')
+         return err
+      }
    }
    render(){
       return(
