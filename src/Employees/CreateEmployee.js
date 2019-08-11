23bdf8d304eca60daea491dd4c00ea94a39d7c1b
dotkalm <joelmholmberg@gmail.com>
2019-08-11 13:48:39 -0700

    edit routes + autofill date form

--- src/Employees/CreateEmployee.js
+++ src/Employees/CreateEmployee.js
@@ -14,8 +14,10 @@ class CreateEmployee extends Component {
    }
    
    render(){
+
       return (
          <form onSubmit={this.props.addEmployee.bind(null, this.state)}>
+            
             <label htmlFor='name'>Employee Name:
                <input type='text' name='name' onChange={this.updateEmployee} value={this.state.name}/>
             </label>
--- src/Employees/EditEmployee.js
+++ src/Employees/EditEmployee.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {Form, Button, Label} from 'semantic-ui-react';
 
 const EditEmployee = (props) => {
+   const formDate = (props.employeeToEdit.birthDate).slice(0,10)
+   console.log(formDate, typeof formDate)
    return (
       <div>
          
@@ -14,6 +16,14 @@ const EditEmployee = (props) => {
             <Label>
                edit Position:
                <Form.Input type='text' name='position' onChange={props.handleFormChange} value={props.employeeToEdit.position}/>
+            </Label>
+            <Label>
+               edit birthday:
+               <Form.Input type='date' name='birthDate' onChange={props.handleFormChange} value={formDate}/>
+            </Label>
+            <Label>
+               edit annual salary:
+               <Form.Input type='text' name='annualSalary' onChange={props.handleFormChange} value={props.employeeToEdit.annualSalary}/>
             </Label>
             <Button type='Submit'>Save Changes</Button>
          </Form>
