ec3bd564df396bf05ea4252ae518aefb7059ae2c
Olvin <olvin.bolanos@yahoo.com>
2019-08-11 09:01:24 -0700

    making changes to the alert page

--- src/Alert/index.jsx
+++ src/Alert/index.jsx
@@ -1,34 +1,6 @@
 import React, {Component} from 'react'
 import {useState} from 'react'
 
-// class Alert extends Component {
-//   state = {
-//     isVisible: false
-//   };
-//   handleToggleState = () => {
-//     this.setState(prevState => ({
-//       isVisible: !prevState.isVisible
-//     }));
-//   };
-//   render() {
-//     return this.state.isVisible ? (
-//       <div className="alert">
-//         <div className="ui error message" style={{display: 'block', color: 'black'}}>{this.props.message}</div>
-//         <button className="ui animated button" type="submit" onClick={this.handleToggleState} >
-//           <div className="visible content">Close Warning</div>
-//             <div className="hidden content">
-//               <i aria-hidden="true" className="window close outline icon" style={{fontSize: '2em'}}></i>
-//             </div>
-//         </button>
-//       </div>
-//     ) : null;
-//   }
-// }
-
-// export default Alert
-
-
-
 class Alert extends Component {
     constructor(props) {
         super(props); 
@@ -39,18 +11,18 @@ class Alert extends Component {
     }
     
     componentWillReceiveProps(e) {
+        console.log(e)
         this.setState(e)
-    }
+    }   
 
     handleToggleState = () => {
       this.setState(prevState => ({
         isVisible: !prevState.isVisible,
-        message: ''
+        message: '',
       }));
-      this.setMessage('')
+      
     };
     render() {
-    // const [message, setMessage] = useState(this.state.message);
 
       return this.state.message ? (
         <div className="alert">
