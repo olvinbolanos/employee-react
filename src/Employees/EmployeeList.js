d9547fe84b3f18bb9cf898c9f74dd41699a400cb
dotkalm <joelmholmberg@gmail.com>
2019-08-12 11:42:17 -0700

    syncs api with employee name

--- src/Employees/EmployeeList.js
+++ src/Employees/EmployeeList.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from 'constants';
 
 
 const Employees = (props) => {
-   console.log(props, '<--- props in employee list')
+   console.log(props, '<---propszzzzzzzz')
    const employeesList = props.employees.map((employee) => {
+      
+      
       const dob = `${new Date(employee.birthDate)}`
       return (
          <li key={employee._id}>
+            <img src={employee.thumb}/><br/>
             <span>Employee Name: {employee.name}</span><br/>
             <span>Position: {employee.position}</span><br/>
             <span>Dept: {employee.department}</span><br/>
             <span>DOB: {dob.replace(/.{42}$/,'').replace(/^.{4}/,'')}</span><br/>
             <span>Annual Salary: {employee.annualSalary}</span><br/>
             <button onClick={props.showModal.bind(null, employee)}>Edit Employee</button>
-            
+            {console.log(employee, '<----thumb')}
          </li>
       )
    })
