e7daa82e3310755286bf796a17f7811e81e0fa05
dotkalm <joelmholmberg@gmail.com>
2019-08-12 13:43:19 -0700

    adds delete button

--- src/Employees/EmployeeList.js
+++ src/Employees/EmployeeList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from 'constants';
+// import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from 'constants';
 
 
 const Employees = (props) => {
@@ -9,24 +9,24 @@ const Employees = (props) => {
       
       const dob = `${new Date(employee.birthDate)}`
       return (
-         <li key={employee._id}>
-            <img src={employee.thumb}/><br/>
+         <span key={employee._id}>
+            <img src={employee.thumb} alt={employee.name}/><br/>
             <span>Employee Name: {employee.name}</span><br/>
             <span>Position: {employee.position}</span><br/>
             <span>Dept: {employee.department}</span><br/>
             <span>DOB: {dob.replace(/.{42}$/,'').replace(/^.{4}/,'')}</span><br/>
             <span>Annual Salary: {employee.annualSalary}</span><br/>
             <button onClick={props.showModal.bind(null, employee)}>Edit Employee</button>
-            {console.log(employee, '<----thumb')}
-         </li>
+            <button onClick={props.deleteEmployee.bind(null, employee._id)}>delete</button><br></br>
+         </span>
       )
    })
    
    return( 
    <div>
-      <ul>
+      <div>
          {employeesList}
-      </ul>
+      </div>
    </div>
    )
 }
